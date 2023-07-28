d38dd5898f5827ef28b41bb635b45fc665e5007d
Julius Aka <aka.julius@hotmail.de>
2023-07-28 12:51:45 +0200

    Update step4c_btms_sizing_sensitivity_wait_time.py

--- simulationScripts/advancedScenario/step4c_btms_sizing_sensitivity_wait_time.py
+++ simulationScripts/advancedScenario/step4c_btms_sizing_sensitivity_wait_time.py
@@ -91,7 +91,7 @@ def do_sizing(iterable):
 
 if __name__ == '__main__':
     
-    a_cost_sizing = np.array([8, 10]) / (365/12)
+    a_cost_sizing = np.array([2,5,8, 10]) / (365/12)
     #d_wait_cost = np.array([1, 5, 10, 15, 20])
     d_wait_cost = np.array(['varying'])
     
